4d665f4bbf0e5e54ddf48a50eca5f20cc7f0fc44
web-novice-tech <itb4769we@gmail.com>
2022-07-20 13:34:56 +0900

    DBの読み書きに使用するタイムゾーンを:utcに設定

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module App
     config.time_zone = ENV["TZ"]
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # DBの読み書きに使用するタイムゾーン
+    config.active_record.default_timezone = :utc
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
