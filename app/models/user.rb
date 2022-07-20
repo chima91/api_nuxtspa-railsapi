5222526aebc7f22ecb362365222450d60bf28451
web-novice-tech <itb4769we@gmail.com>
2022-07-20 17:23:16 +0900

    nameとpassword属性のvalidatesを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+  # bcrypt gem の メソッド（新規登録時のみパスワード入力必須を検証する）
+  has_secure_password
+
+  validates :name, presence: true,
+                    length: {
+                      maximum: 30,
+                      # 入力が空白の場合にはエラーを出さない
+                      allow_blank: true
+                    }
+
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+                        length: {
+                          minimum: 8,
+                          allow_blank: true
+                        },
+                        format: {
+                          with: VALID_PASSWORD_REGEX,
+                          allow_blank: true
+                        },
+                        # name属性のみ更新する場合などにエラーとならないために設定する
+                        allow_nil: true
 end
