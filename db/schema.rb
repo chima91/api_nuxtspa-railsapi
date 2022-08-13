ad51e35e2752b92ba895aa59d01207c11340c493
web-novice-tech <itb4769we@gmail.com>
2022-08-14 08:23:15 +0900

    リフレッシュトークンを発行するRefreshToken classの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_075250) do
+ActiveRecord::Schema.define(version: 2022_08_13_230302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_07_20_075250) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "refresh_jti"
   end
 
 end
