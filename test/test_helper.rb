9b2c33508bc397141c542db4953db7debb5da30c
web-novice-tech <itb4769we@gmail.com>
2022-08-14 10:19:05 +0900

    AuthTokenControllerのテストコードを実装

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,21 +1,49 @@
 ENV['RAILS_ENV'] ||= 'test'
-require_relative "../config/environment"
-require "rails/test_help"
+require_relative '../config/environment'
+require 'rails/test_help'
 
+# gem minitest-reporters setup
 require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+  # プロセスが分岐した直後に呼び出し
   parallelize_setup do |worker|
     # seedデータの読み込み
     load "#{Rails.root}/db/seeds.rb"
   end
 
-  # :number_of_processors -> 使用しているマシン(今回はDocker)のコア数 -> 4
+  # 並列テストの有効化・無効化
   parallelize(workers: :number_of_processors)
 
-  # アクティブユーザーを返す
+  # アクティブなユーザーを返す
   def active_user
     User.find_by(activated: true)
   end
+
+  # api path
+  def api(path = "/")
+    "/api/v1#{path}"
+  end
+
+  # 認可ヘッダ
+  def auth(token)
+    { Authorization: "Bearer #{token}" }
+  end
+
+  # 引数のparamsでログインを行う
+  def login(params)
+    post api("/auth_token"), xhr: true, params: params
+  end
+
+  # ログアウトapi
+  def logout
+    delete api("/auth_token"), xhr: true
+  end
+
+  # レスポンスJSONをハッシュで返す
+  def res_body
+    JSON.parse(@response.body)
+  end
+
 end
